b049b5ec20360c668643eb47fba2a7b26e94ff1f
Andy Baxter <andrewbaxter439@gmail.com>
2025-10-08 12:43:18 +0100

    accept seed in euromod_runs

--- R/run_euromod_runs.R
+++ R/run_euromod_runs.R
@@ -1,4 +1,22 @@
-here::here("increasing_employment_rates.qmd")
+args <- commandArgs(trailingOnly = FALSE)
+
+if ("--seed" %in% args) {
+  seed <- as.integer(args[which(args == "--seed") + 1])
+  cat("setting seed to ", seed, "\n")
+  set.seed(seed)
+} else if ("-s" %in% args){
+  seed <- as.integer(args[which(args == "-s") + 1])
+  cat("setting seed to ", seed, "\n")
+  set.seed(seed)
+} else {
+  set.seed(150)
+}
+
+seed_addin <- ""
+
+if ("--seed" %in% args | "-s" %in% args) seed_addin <- glue::glue("_{seed}")
+
+here::here("R", "run_euromod_runs.R")
 
 library(reticulate)
 library(tidyverse)
@@ -14,34 +32,36 @@ min_year <- 2026
 max_year <- 2026
 
 ## ----ukmod-uprated-pop-5pc-----------------------------------------------------------------------------------------------------------
-
-data_5pc <- read_tsv(here("data/matching_updated_pop_5pc.txt"))
-
-if (!dir.exists("output/uprated_5pc")) dir.create("output/uprated_5pc")
-
-walk(min_year:max_year, \(year) {
-  policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
-  policy_system$run(
-    data_5pc,
-    "UK_2023_b1.txt",
-    outputpath = here("output/uprated_5pc")
-  )
-}, .progress = TRUE)
+# 
+# data_5pc <- read_tsv(here(glue::glue("data/matching_updated_pop_5pc{seed_addin}.txt")))
+# 
+# if (!dir.exists("output/uprated_5pc"))) dir.create("output/uprated_5pc")
+# 
+# walk(min_year:max_year, \(year) {
+#   policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
+#   policy_system$run(
+#     data_5pc,
+#     "UK_2023_b1.txt",
+#     outputpath = here("output/uprated_5pc")
+#   )
+# }, .progress = TRUE)
 
 
 
 ## ----ukmod-uprated-seeking_u25-5pc---------------------------------------------------------------------------------------------------
 
-data_seeking_u25_5pc <- read_tsv(here("data/matching_updated_pop_seeking_u25_5pc.txt"))
+data_seeking_u25_5pc <- read_tsv(here(glue::glue("data/matching_updated_pop_seeking_u25_5pc{seed_addin}.txt")))
 
-if (!dir.exists("output/uprated_seeking_u25_5pc")) dir.create("output/uprated_seeking_u25_5pc")
+dir_out <- glue::glue("output/uprated_seeking_u25_5pc{seed_addin}")
+
+if (!dir.exists(dir_out)) dir.create(dir_out)
 
 walk(min_year:max_year, \(year) {
   policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
   policy_system$run(
     data_seeking_u25_5pc,
     "UK_2023_b1.txt",
-    outputpath = here("output/uprated_seeking_u25_5pc")
+    outputpath = here(dir_out)
   )
 }, .progress = TRUE)
 
@@ -49,16 +69,18 @@ walk(min_year:max_year, \(year) {
 
 ## ----ukmod-uprated-seeking_o25-5pc---------------------------------------------------------------------------------------------------
 
-data_seeking_o25_5pc <- read_tsv(here("data/matching_updated_pop_seeking_o25_5pc.txt"))
+data_seeking_o25_5pc <- read_tsv(here(glue::glue("data/matching_updated_pop_seeking_o25_5pc{seed_addin}.txt")))
 
-if (!dir.exists("output/uprated_seeking_o25_5pc")) dir.create("output/uprated_seeking_o25_5pc")
+dir_out <- glue::glue("output/uprated_seeking_o25_5pc{seed_addin}")
+
+if (!dir.exists(dir_out)) dir.create(dir_out)
 
 walk(min_year:max_year, \(year) {
   policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
   policy_system$run(
     data_seeking_o25_5pc,
     "UK_2023_b1.txt",
-    outputpath = here("output/uprated_seeking_o25_5pc")
+    outputpath = here(dir_out)
   )
 }, .progress = TRUE)
 
@@ -66,16 +88,18 @@ walk(min_year:max_year, \(year) {
 
 ## ----ukmod-uprated-sick_or_disabled_u25-5pc------------------------------------------------------------------------------------------
 
-data_sick_or_disabled_u25_5pc <- read_tsv(here("data/matching_updated_pop_sick_or_disabled_u25_5pc.txt"))
+data_sick_or_disabled_u25_5pc <- read_tsv(here(glue::glue("data/matching_updated_pop_sick_or_disabled_u25_5pc{seed_addin}.txt")))
 
-if (!dir.exists("output/uprated_sick_or_disabled_u25_5pc")) dir.create("output/uprated_sick_or_disabled_u25_5pc")
+dir_out <- glue::glue("output/uprated_sick_or_disabled_u25_5pc{seed_addin}")
+
+if (!dir.exists(dir_out)) dir.create(dir_out)
 
 walk(min_year:max_year, \(year) {
   policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
   policy_system$run(
     data_sick_or_disabled_u25_5pc,
     "UK_2023_b1.txt",
-    outputpath = here("output/uprated_sick_or_disabled_u25_5pc")
+    outputpath = here(dir_out)
   )
 }, .progress = TRUE)
 
@@ -83,16 +107,18 @@ walk(min_year:max_year, \(year) {
 
 ## ----ukmod-uprated-sick_or_disabled_o25-5pc------------------------------------------------------------------------------------------
 
-data_sick_or_disabled_o25_5pc <- read_tsv(here("data/matching_updated_pop_sick_or_disabled_o25_5pc.txt"))
+data_sick_or_disabled_o25_5pc <- read_tsv(here(glue::glue("data/matching_updated_pop_sick_or_disabled_o25_5pc{seed_addin}.txt")))
 
-if (!dir.exists("output/uprated_sick_or_disabled_o25_5pc")) dir.create("output/uprated_sick_or_disabled_o25_5pc")
+dir_out <- glue::glue("output/uprated_sick_or_disabled_o25_5pc{seed_addin}")
+
+if (!dir.exists(dir_out)) dir.create(dir_out)
 
 walk(min_year:max_year, \(year) {
   policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
   policy_system$run(
     data_sick_or_disabled_o25_5pc,
     "UK_2023_b1.txt",
-    outputpath = here("output/uprated_sick_or_disabled_o25_5pc")
+    outputpath = here(dir_out)
   )
 }, .progress = TRUE)
 
