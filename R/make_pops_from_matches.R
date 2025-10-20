1209daf1d523c5751d2c6b13da5b672488b1a05e
Andy Baxter <andrewbaxter439@gmail.com>
2025-10-20 15:33:12 +0100

    making things functions

--- R/make_pops_from_matches.R
+++ R/make_pops_from_matches.R
@@ -1,234 +1,242 @@
-args <- commandArgs(trailingOnly = FALSE)
-
-if ("--seed" %in% args) {
-  seed <- as.integer(args[which(args == "--seed") + 1])
-  cat("setting seed to ", seed, "\n")
-  set.seed(seed)
-} else if ("-s" %in% args){
-  seed <- as.integer(args[which(args == "-s") + 1])
-  cat("setting seed to ", seed, "\n")
-  set.seed(seed)
-} else {
-  set.seed(150)
-}
-
 # Read in data ------------------------------------------------------------
 
-here::here("increasing_employment_rates.qmd")
-
-library(reticulate)
-library(tidyverse)
-library(here)
-library(fixest)
-
-
-
-raw_data <- readr::read_tsv(here("data/UK_2023_b1.txt"))
-
-
-data <- raw_data |>
-  ## Group ages and ethnicity_collapsed
-  mutate(
-    age_collapsed = case_when(
-      dag >= 16 & dag < 25 ~ 1,
-      dag >= 25 & dag < 31 ~ 2,
-      dag >= 31 & dag < 51 ~ 3,
-      dag >= 51 & dag < 56 ~ 4,
-      dag >= 56 ~ 5
-    ),
-    ethnicity_collapsed = case_when(
-      dot %in% 1:4 ~ "White",
-      dot %in% 5:8 ~ "Mixed",
-      dot %in% 9:13 ~ "Asian",
-      dot %in% 14:16 ~ "Black",
-      dot == 17 ~ "Other",
-      .default = "Missing"
-    ),
-    dms_collapsed = case_when(
-      dms == 1 ~ "single",
-      dms == 2 ~ "married",
-      dms %in% 3:5 ~ "separated",
-      .default = "single"
-    ),
-    deh_c3 = fct_collapse(
-      factor(deh),
-      "Low" = c(0:1),
-      "Med" = c(2:4),
-      "High" = 5
-    ),
-    enter_employment = 0
-  ) |>
-  mutate(n_ch = sum(dag < 17), .by = idhh) |>
-  mutate(
-    n_ch = case_when(n_ch == 0 ~ "0", n_ch == 1 ~ "1", n_ch > 1 ~ "2+"),
-    unemployed = case_when(les == 3 ~ 0L, les %in% c(5, 7, 8) ~ 1L, .default = NA_integer_)
-  )
-
-employed_pop <- data |>
-  ## All employed
-  filter(les == 3, yem != 0, lhw != 0)
-
-not_in_employment_pop <-  data |>
-  ## Select unemployed, inactive, sick or disabled, working age
-  filter(les %in% c(5, 7, 8), dag %in% 16:64, !is.na(dag)) |>
-  mutate(
-    seeking = les == 5,
-    sick_or_disabled = les == 8,
-    under_25 = age_collapsed == 1,
-    enter_employment = 1
-  )
-
-
-
-
-# progressive matching ----------------------------------------------------
-
-
-matches <- employed_pop |>
-  select(
-    les,
-    lhw,
-    yem,
-    yds,
-    bch,
-    bsa,
-    bho,
-    lindi,
-    lfs,
-    age_collapsed, # age group
-    dgn, # gender
-    deh_c3, # education - 3 cat
-    dms_collapsed, # marital status
-    n_ch, # n kids (0, 1, or 2+)
-    drgn1, # area
-    dhr, # home responsible
-    ddi03, # disabled
-    lcr01,  # carer
-    ethnicity_collapsed, # ethnicity
-  )
-
-unmatched_pop <- not_in_employment_pop |>
-  select(-c(les, lhw, yem, yds, bch, bsa, bho, lindi, lfs))
-
-find_matches <- function(unmatched_pop, matches = matches) {
+make_pops_from_matches <- function(filename = "data/UK_2023_b1.txt", seed = 150, cli_args = NULL) {
   
-  if(nrow(unmatched_pop) == 0) {
-    return(unmatched_pop)
-  }
+  set.seed(seed)
   
-  cat("Still to match", nrow(unmatched_pop), "rows\n")
+  here::here("increasing_employment_rates.qmd")
   
-  if(!("age_collapsed" %in% colnames(matches))) {
-    browser()
-    stop("No more matches to be found!")
-  }
+  library(reticulate)
+  library(tidyverse)
+  library(here)
+  library(fixest)
   
-  rolling_match <- unmatched_pop |> 
-    left_join(matches, relationship = "many-to-many") 
   
-  matched <- rolling_match |> 
-    filter(n() >=5, .by = idperson)
   
-  cat("Matched", nrow(matched), "rows in this iteration\n")
+  raw_data <- readr::read_tsv(filename)
   
-  matched |> 
-    bind_rows(
-      unmatched_pop |>
-        filter(!(idperson %in% matched$idperson)) |> 
-        find_matches(matches |> select(-last_col()))
+  
+  data <- raw_data |>
+    ## Group ages and ethnicity_collapsed
+    mutate(
+      age_collapsed = case_when(
+        dag >= 16 & dag < 25 ~ 1,
+        dag >= 25 & dag < 31 ~ 2,
+        dag >= 31 & dag < 51 ~ 3,
+        dag >= 51 & dag < 56 ~ 4,
+        dag >= 56 ~ 5
+      ),
+      ethnicity_collapsed = case_when(
+        dot %in% 1:4 ~ "White",
+        dot %in% 5:8 ~ "Mixed",
+        dot %in% 9:13 ~ "Asian",
+        dot %in% 14:16 ~ "Black",
+        dot == 17 ~ "Other",
+        .default = "Missing"
+      ),
+      dms_collapsed = case_when(
+        dms == 1 ~ "single",
+        dms == 2 ~ "married",
+        dms %in% 3:5 ~ "separated",
+        .default = "single"
+      ),
+      deh_c3 = fct_collapse(
+        factor(deh),
+        "Low" = c(0:1),
+        "Med" = c(2:4),
+        "High" = 5
+      ),
+      enter_employment = 0
+    ) |>
+    mutate(n_ch = sum(dag < 17), .by = idhh) |>
+    mutate(
+      n_ch = case_when(n_ch == 0 ~ "0", n_ch == 1 ~ "1", n_ch > 1 ~ "2+"),
+      unemployed = case_when(les == 3 ~ 0L, les %in% c(5, 7, 8) ~ 1L, .default = NA_integer_)
     )
-}
-
-output_matches <- find_matches(unmatched_pop, matches)
-
-
-replacement_workers <- output_matches |> 
-  slice_sample(n = 1, by = idperson) |> 
-  select(-c(
+  
+  employed_pop <- data |>
+    ## All employed
+    filter(les == 3, yem != 0, lhw != 0)
+  
+  not_in_employment_pop <-  data |>
+    ## Select unemployed, inactive, sick or disabled, working age
+    filter(les %in% c(5, 7, 8), dag %in% 16:64, !is.na(dag)) |>
+    mutate(
+      seeking = les == 5,
+      sick_or_disabled = les == 8,
+      under_25 = age_collapsed == 1,
+      enter_employment = 1
+    )
+  
+  
+  
+  
+  # progressive matching ----------------------------------------------------
+  
+  
+  matches <- employed_pop |>
+    select(
+      les,
+      lhw,
+      yem,
+      yds,
+      bch,
+      bsa,
+      bho,
+      lindi,
+      lfs,
+      age_collapsed, # age group
+      dgn, # gender
+      deh_c3, # education - 3 cat
+      dms_collapsed, # marital status
+      n_ch, # n kids (0, 1, or 2+)
+      drgn1, # area
+      dhr, # home responsible
+      ddi03, # disabled
+      lcr01,  # carer
+      ethnicity_collapsed, # ethnicity
+    )
+  
+  unmatched_pop <- not_in_employment_pop |>
+    select(-c(les, lhw, yem, yds, bch, bsa, bho, lindi, lfs))
+  
+  find_matches <- function(unmatched_pop, matches = matches) {
+    
+    if(nrow(unmatched_pop) == 0) {
+      return(unmatched_pop)
+    }
+    
+    cat("Still to match", nrow(unmatched_pop), "rows\n")
+    
+    if(!("age_collapsed" %in% colnames(matches))) {
+      browser()
+      stop("No more matches to be found!")
+    }
+    
+    rolling_match <- unmatched_pop |> 
+      left_join(matches, relationship = "many-to-many") 
+    
+    matched <- rolling_match |> 
+      filter(n() >=5, .by = idperson)
+    
+    cat("Matched", nrow(matched), "rows in this iteration\n")
+    
+    matched |> 
+      bind_rows(
+        unmatched_pop |>
+          filter(!(idperson %in% matched$idperson)) |> 
+          find_matches(matches |> select(-last_col()))
+      )
+  }
+  
+  output_matches <- find_matches(unmatched_pop, matches)
+  
+  
+  replacement_workers <- output_matches |> 
+    slice_sample(n = 1, by = idperson) |> 
+    select(-c(
       n_ch,
       age_collapsed,
       dms_collapsed,
       ethnicity_collapsed,
       deh_c3
     )
-  )
-
-## ----create-samples------------------------------------------------------------------------------------------------------------------
-
-slice_weighted_sample <- function(data = replacement_workers, ..., prop = 0.05) {
+    )
   
-  data |> 
-    filter(...) |> 
-    slice_sample(prop = 1) |> 
-    mutate(.cum_dwt = cumsum(dwt)/sum(dwt)) |> 
-    filter(.cum_dwt <= prop) |> 
-    select(-.cum_dwt)
+  ## ----create-samples------------------------------------------------------------------------------------------------------------------
+  
+  slice_weighted_sample <- function(data = replacement_workers, ..., prop = 0.05) {
+    
+    data |> 
+      filter(...) |> 
+      slice_sample(prop = 1) |> 
+      mutate(.cum_dwt = cumsum(dwt)/sum(dwt)) |> 
+      filter(.cum_dwt <= prop) |> 
+      select(-.cum_dwt)
+    
+  }
+  
+  # sample_workers_seeking <- replacement_workers |>
+  #   slice_weighted_sample(seeking)
+  # 
+  # cat(glue::glue("Workers entering employment: {sum(sample_workers_seeking$dwt)}"), "\n\n")
+  
+  sample_workers_seeking_u25 <- replacement_workers |> 
+    slice_weighted_sample(seeking, under_25)
+  
+  cat(glue::glue("Under 25, seeking entering employment: {sum(sample_workers_seeking_u25$dwt)}"), "\n\n")
+  
+  sample_workers_seeking_o25 <- replacement_workers |> 
+    slice_weighted_sample(seeking, !under_25)
+  
+  cat(glue::glue("25 and over, seeking entering employment: {sum(sample_workers_seeking_o25$dwt)}"), "\n\n")
+  
+  sample_workers_sick_or_disabled_u25 <- replacement_workers |> 
+    slice_weighted_sample(sick_or_disabled, under_25)
+  
+  cat(glue::glue("Under 25, disabled entering employment: {sum(sample_workers_sick_or_disabled_u25$dwt)}"), "\n\n")
+  
+  sample_workers_sick_or_disabled_o25 <- replacement_workers |> 
+    slice_weighted_sample(sick_or_disabled, !under_25)
+  
+  cat(glue::glue("25 and over, disabled entering employment: {sum(sample_workers_sick_or_disabled_o25$dwt)}"), "\n\n")
+  
+  # uprated_pop_5pc <- raw_data |> 
+  #   filter(!(idperson %in% sample_workers_seeking$idperson)) |> 
+  #   mutate(enter_employment = 0) |> 
+  #   bind_rows(sample_workers_seeking) |> 
+  #   arrange(idhh, idperson)
+  
+  uprated_seeking_u25_5pc <- raw_data |> 
+    filter(!(idperson %in% sample_workers_seeking_u25$idperson)) |> 
+    mutate(enter_employment = 0) |> 
+    bind_rows(sample_workers_seeking_u25) |> 
+    arrange(idhh, idperson)
+  
+  uprated_seeking_o25_5pc <- raw_data |> 
+    filter(!(idperson %in% sample_workers_seeking_o25$idperson)) |> 
+    mutate(enter_employment = 0) |> 
+    bind_rows(sample_workers_seeking_o25) |> 
+    arrange(idhh, idperson)
+  
+  uprated_sick_or_disabled_u25_5pc <- raw_data |> 
+    filter(!(idperson %in% sample_workers_sick_or_disabled_u25$idperson)) |> 
+    mutate(enter_employment = 0) |> 
+    bind_rows(sample_workers_sick_or_disabled_u25) |> 
+    arrange(idhh, idperson)
+  
+  uprated_sick_or_disabled_o25_5pc <- raw_data |> 
+    filter(!(idperson %in% sample_workers_sick_or_disabled_o25$idperson)) |> 
+    mutate(enter_employment = 0) |> 
+    bind_rows(sample_workers_sick_or_disabled_o25) |> 
+    arrange(idhh, idperson)
+  
+  seed_addin <- ""
+  
+  if (!is.null(cli_args)) seed_addin <- glue::glue("_{seed}")
+  
+  # write_tsv(uprated_pop_5pc, glue::glue("data/matching_updated_pop_5pc{seed_addin}.txt"))
+  write_tsv(uprated_seeking_u25_5pc, glue::glue("data/matching_updated_pop_seeking_u25_5pc{seed_addin}.txt"))
+  write_tsv(uprated_seeking_o25_5pc, glue::glue("data/matching_updated_pop_seeking_o25_5pc{seed_addin}.txt"))
+  write_tsv(uprated_sick_or_disabled_u25_5pc, glue::glue("data/matching_updated_pop_sick_or_disabled_u25_5pc{seed_addin}.txt"))
+  write_tsv(uprated_sick_or_disabled_o25_5pc, glue::glue("data/matching_updated_pop_sick_or_disabled_o25_5pc{seed_addin}.txt"))
+  
+  return("matching_updated_pop")
   
 }
 
-# sample_workers_seeking <- replacement_workers |>
-#   slice_weighted_sample(seeking)
-# 
-# cat(glue::glue("Workers entering employment: {sum(sample_workers_seeking$dwt)}"), "\n\n")
-
-sample_workers_seeking_u25 <- replacement_workers |> 
-  slice_weighted_sample(seeking, under_25)
-
-cat(glue::glue("Under 25, seeking entering employment: {sum(sample_workers_seeking_u25$dwt)}"), "\n\n")
-
-sample_workers_seeking_o25 <- replacement_workers |> 
-  slice_weighted_sample(seeking, !under_25)
-
-cat(glue::glue("25 and over, seeking entering employment: {sum(sample_workers_seeking_o25$dwt)}"), "\n\n")
-
-sample_workers_sick_or_disabled_u25 <- replacement_workers |> 
-  slice_weighted_sample(sick_or_disabled, under_25)
-
-cat(glue::glue("Under 25, disabled entering employment: {sum(sample_workers_sick_or_disabled_u25$dwt)}"), "\n\n")
-
-sample_workers_sick_or_disabled_o25 <- replacement_workers |> 
-  slice_weighted_sample(sick_or_disabled, !under_25)
-
-cat(glue::glue("25 and over, disabled entering employment: {sum(sample_workers_sick_or_disabled_o25$dwt)}"), "\n\n")
-
-# uprated_pop_5pc <- raw_data |> 
-#   filter(!(idperson %in% sample_workers_seeking$idperson)) |> 
-#   mutate(enter_employment = 0) |> 
-#   bind_rows(sample_workers_seeking) |> 
-#   arrange(idhh, idperson)
-
-uprated_seeking_u25_5pc <- raw_data |> 
-  filter(!(idperson %in% sample_workers_seeking_u25$idperson)) |> 
-  mutate(enter_employment = 0) |> 
-  bind_rows(sample_workers_seeking_u25) |> 
-  arrange(idhh, idperson)
-
-uprated_seeking_o25_5pc <- raw_data |> 
-  filter(!(idperson %in% sample_workers_seeking_o25$idperson)) |> 
-  mutate(enter_employment = 0) |> 
-  bind_rows(sample_workers_seeking_o25) |> 
-  arrange(idhh, idperson)
-
-uprated_sick_or_disabled_u25_5pc <- raw_data |> 
-  filter(!(idperson %in% sample_workers_sick_or_disabled_u25$idperson)) |> 
-  mutate(enter_employment = 0) |> 
-  bind_rows(sample_workers_sick_or_disabled_u25) |> 
-  arrange(idhh, idperson)
-
-uprated_sick_or_disabled_o25_5pc <- raw_data |> 
-  filter(!(idperson %in% sample_workers_sick_or_disabled_o25$idperson)) |> 
-  mutate(enter_employment = 0) |> 
-  bind_rows(sample_workers_sick_or_disabled_o25) |> 
-  arrange(idhh, idperson)
-
-seed_addin <- ""
-
-if ("--seed" %in% args | "-s" %in% args) seed_addin <- glue::glue("_{seed}")
-
-# write_tsv(uprated_pop_5pc, glue::glue("data/matching_updated_pop_5pc{seed_addin}.txt"))
-write_tsv(uprated_seeking_u25_5pc, glue::glue("data/matching_updated_pop_seeking_u25_5pc{seed_addin}.txt"))
-write_tsv(uprated_seeking_o25_5pc, glue::glue("data/matching_updated_pop_seeking_o25_5pc{seed_addin}.txt"))
-write_tsv(uprated_sick_or_disabled_u25_5pc, glue::glue("data/matching_updated_pop_sick_or_disabled_u25_5pc{seed_addin}.txt"))
-write_tsv(uprated_sick_or_disabled_o25_5pc, glue::glue("data/matching_updated_pop_sick_or_disabled_o25_5pc{seed_addin}.txt"))
-
-
+# if (!interactive()) {
+#   args <- commandArgs(trailingOnly = FALSE)
+#   
+#   if ("--seed" %in% args) {
+#     seed <- as.integer(args[which(args == "--seed") + 1])
+#     cat("setting seed to ", seed, "\n")
+#   } else if ("-s" %in% args){
+#     seed <- as.integer(args[which(args == "-s") + 1])
+#     cat("setting seed to ", seed, "\n")
+#   } else {
+#     seed <- 150
+#   }
+#   
+#   make_pops_from_matches(seed = seed, cli_args = args)
+# }
--- R/run_euromod_runs.R
+++ R/run_euromod_runs.R
@@ -1,124 +1,133 @@
-args <- commandArgs(trailingOnly = FALSE)
-
-if ("--seed" %in% args) {
-  seed <- as.integer(args[which(args == "--seed") + 1])
-  cat("setting seed to ", seed, "\n")
-  set.seed(seed)
-} else if ("-s" %in% args){
-  seed <- as.integer(args[which(args == "-s") + 1])
-  cat("setting seed to ", seed, "\n")
-  set.seed(seed)
-} else {
-  set.seed(150)
+run_euromod <- function(seed = NULL, min_year = 2026, max_year = 2026, match_files = "matching_uprated_pop", .model = "UKMOD-PUBLIC-B2025.07") {
+  
+  
+  
+  seed_addin <- ""
+  
+  if (!is.null(seed)) {
+    seed_addin <- glue::glue("_{seed}")
+    set.seed(seed)
+  }
+  
+  here::here("R", "run_euromod_runs.R")
+  
+  library(reticulate)
+  library(tidyverse)
+  library(here)
+  
+  euromod <- import("euromod")
+  
+  
+  mod <- euromod$Model(here(.model))
+  
+  ## ----ukmod-uprated-pop-5pc-----------------------------------------------------------------------------------------------------------
+  # 
+  # data_5pc <- read_tsv(here(glue::glue("data/{match_files}_5pc{seed_addin}.txt")))
+  # 
+  # if (!dir.exists("output/uprated_5pc"))) dir.create("output/uprated_5pc")
+  # 
+  # walk(min_year:max_year, \(year) {
+  #   policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
+  #   policy_system$run(
+  #     data_5pc,
+  #     "UK_2023_b1.txt",
+  #     outputpath = here("output/uprated_5pc")
+  #   )
+  # }, .progress = TRUE)
+  
+  
+  
+  ## ----ukmod-uprated-seeking_u25-5pc---------------------------------------------------------------------------------------------------
+  
+  data_seeking_u25_5pc <- read_tsv(here(glue::glue("data/{match_files}_seeking_u25_5pc{seed_addin}.txt")))
+  
+  dir_out <- as.character(glue::glue("output/uprated_seeking_u25_5pc{seed_addin}"))
+  
+  if (!dir.exists(dir_out)) dir.create(dir_out)
+  
+  walk(min_year:max_year, \(year) {
+    policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
+    policy_system$run(
+      data_seeking_u25_5pc,
+      "UK_2023_b1.txt",
+      outputpath = here(dir_out)
+    )
+  }, .progress = TRUE)
+  
+  
+  
+  ## ----ukmod-uprated-seeking_o25-5pc---------------------------------------------------------------------------------------------------
+  
+  data_seeking_o25_5pc <- read_tsv(here(glue::glue("data/{match_files}_seeking_o25_5pc{seed_addin}.txt")))
+  
+  dir_out <- as.character(glue::glue("output/uprated_seeking_o25_5pc{seed_addin}"))
+  
+  if (!dir.exists(dir_out)) dir.create(dir_out)
+  
+  walk(min_year:max_year, \(year) {
+    policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
+    policy_system$run(
+      data_seeking_o25_5pc,
+      "UK_2023_b1.txt",
+      outputpath = here(dir_out)
+    )
+  }, .progress = TRUE)
+  
+  
+  
+  ## ----ukmod-uprated-sick_or_disabled_u25-5pc------------------------------------------------------------------------------------------
+  
+  data_sick_or_disabled_u25_5pc <- read_tsv(here(glue::glue("data/{match_files}_sick_or_disabled_u25_5pc{seed_addin}.txt")))
+  
+  dir_out <- as.character(glue::glue("output/uprated_sick_or_disabled_u25_5pc{seed_addin}"))
+  
+  if (!dir.exists(dir_out)) dir.create(dir_out)
+  
+  walk(min_year:max_year, \(year) {
+    policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
+    policy_system$run(
+      data_sick_or_disabled_u25_5pc,
+      "UK_2023_b1.txt",
+      outputpath = here(dir_out)
+    )
+  }, .progress = TRUE)
+  
+  
+  
+  ## ----ukmod-uprated-sick_or_disabled_o25-5pc------------------------------------------------------------------------------------------
+  
+  data_sick_or_disabled_o25_5pc <- read_tsv(here(glue::glue("data/{match_files}_sick_or_disabled_o25_5pc{seed_addin}.txt")))
+  
+  dir_out <- as.character(glue::glue("output/uprated_sick_or_disabled_o25_5pc{seed_addin}"))
+  
+  if (!dir.exists(dir_out)) dir.create(dir_out)
+  
+  walk(min_year:max_year, \(year) {
+    policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
+    policy_system$run(
+      data_sick_or_disabled_o25_5pc,
+      "UK_2023_b1.txt",
+      outputpath = here(dir_out)
+    )
+  }, .progress = TRUE)
+  
+  return(TRUE)
+  
 }
 
-seed_addin <- ""
 
-if ("--seed" %in% args | "-s" %in% args) seed_addin <- glue::glue("_{seed}")
-
-here::here("R", "run_euromod_runs.R")
-
-library(reticulate)
-library(tidyverse)
-library(here)
-
-euromod <- import("euromod")
-
-# unzip("UKMOD-PUBLIC-B2025.07.zip")
-
-mod <- euromod$Model(here("UKMOD-PUBLIC-B2025.07"))
-
-min_year <- 2026
-max_year <- 2029
-
-## ----ukmod-uprated-pop-5pc-----------------------------------------------------------------------------------------------------------
-# 
-# data_5pc <- read_tsv(here(glue::glue("data/matching_updated_pop_5pc{seed_addin}.txt")))
-# 
-# if (!dir.exists("output/uprated_5pc"))) dir.create("output/uprated_5pc")
-# 
-# walk(min_year:max_year, \(year) {
-#   policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
-#   policy_system$run(
-#     data_5pc,
-#     "UK_2023_b1.txt",
-#     outputpath = here("output/uprated_5pc")
-#   )
-# }, .progress = TRUE)
-
-
-
-## ----ukmod-uprated-seeking_u25-5pc---------------------------------------------------------------------------------------------------
-
-data_seeking_u25_5pc <- read_tsv(here(glue::glue("data/matching_updated_pop_seeking_u25_5pc{seed_addin}.txt")))
-
-dir_out <- as.character(glue::glue("output/uprated_seeking_u25_5pc{seed_addin}"))
-
-if (!dir.exists(dir_out)) dir.create(dir_out)
-
-walk(min_year:max_year, \(year) {
-  policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
-  policy_system$run(
-    data_seeking_u25_5pc,
-    "UK_2023_b1.txt",
-    outputpath = here(dir_out)
-  )
-}, .progress = TRUE)
-
-
-
-## ----ukmod-uprated-seeking_o25-5pc---------------------------------------------------------------------------------------------------
-
-data_seeking_o25_5pc <- read_tsv(here(glue::glue("data/matching_updated_pop_seeking_o25_5pc{seed_addin}.txt")))
-
-dir_out <- as.character(glue::glue("output/uprated_seeking_o25_5pc{seed_addin}"))
-
-if (!dir.exists(dir_out)) dir.create(dir_out)
-
-walk(min_year:max_year, \(year) {
-  policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
-  policy_system$run(
-    data_seeking_o25_5pc,
-    "UK_2023_b1.txt",
-    outputpath = here(dir_out)
-  )
-}, .progress = TRUE)
-
-
-
-## ----ukmod-uprated-sick_or_disabled_u25-5pc------------------------------------------------------------------------------------------
-
-data_sick_or_disabled_u25_5pc <- read_tsv(here(glue::glue("data/matching_updated_pop_sick_or_disabled_u25_5pc{seed_addin}.txt")))
-
-dir_out <- as.character(glue::glue("output/uprated_sick_or_disabled_u25_5pc{seed_addin}"))
-
-if (!dir.exists(dir_out)) dir.create(dir_out)
-
-walk(min_year:max_year, \(year) {
-  policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
-  policy_system$run(
-    data_sick_or_disabled_u25_5pc,
-    "UK_2023_b1.txt",
-    outputpath = here(dir_out)
-  )
-}, .progress = TRUE)
-
-
-
-## ----ukmod-uprated-sick_or_disabled_o25-5pc------------------------------------------------------------------------------------------
-
-data_sick_or_disabled_o25_5pc <- read_tsv(here(glue::glue("data/matching_updated_pop_sick_or_disabled_o25_5pc{seed_addin}.txt")))
-
-dir_out <- as.character(glue::glue("output/uprated_sick_or_disabled_o25_5pc{seed_addin}"))
-
-if (!dir.exists(dir_out)) dir.create(dir_out)
-
-walk(min_year:max_year, \(year) {
-  policy_system <- mod$countries['UK']$systems[glue::glue("UK_{year}")]
-  policy_system$run(
-    data_sick_or_disabled_o25_5pc,
-    "UK_2023_b1.txt",
-    outputpath = here(dir_out)
-  )
-}, .progress = TRUE)
-
+# if (!interactive()) {
+#   args <- commandArgs(trailingOnly = FALSE)
+#   
+#   if ("--seed" %in% args) {
+#     seed <- as.integer(args[which(args == "--seed") + 1])
+#     cat("setting seed to ", seed, "\n")
+#   } else if ("-s" %in% args){
+#     seed <- as.integer(args[which(args == "-s") + 1])
+#     cat("setting seed to ", seed, "\n")
+#   } else {
+#     seed <- NULL
+#   }
+#   
+#   run_euromod(seed, min_year = 2026, max_year = 2029)
+# }
