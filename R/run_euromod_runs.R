44c06b0664c45705d21efa5cf43ab59d54e6bfe4
Andy Baxter <andrewbaxter439@gmail.com>
2025-10-22 16:19:45 +0100

    attempt make dotnet work

--- R/run_euromod_runs.R
+++ R/run_euromod_runs.R
@@ -15,6 +15,8 @@ run_euromod <- function(seed = NULL, min_year = 2026, max_year = 2026, match_fil
   library(tidyverse)
   library(here)
   
+  source_python("py/init_dotnet.py")
+  
   euromod <- import("euromod")
   
   
